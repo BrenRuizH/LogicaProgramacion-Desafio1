d75b5add5cbd328edfc1d499af80a2d9b0bfdabb
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-03-15 14:55:18 -0600

    2024-03-15 Crear una función que pregunte el nombre de una ciudad de Brasil y después muestre una alerta concatenando la respuesta con el texto

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Hora del Desafío';
 
-function consola() {
+function buttonConsole() {
     alert('El botón fue clicado');
+}
+
+function buttonPrompt() {
+    
+    let ciudad = prompt("Menciona una ciudad de Brasil");
+    alert(`Estuve en ${ciudad} y me acordé de ti`);
+
 }
