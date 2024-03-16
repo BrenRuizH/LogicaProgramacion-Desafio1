803b54228e586f8f12f7e4359ed0a4422c3d6be3
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-03-15 23:43:39 -0600

    2024-03-15 Crea una función que calcule el valor del factorial de un número pasado como parámetro

--- app3.js
+++ app3.js
@@ -3,3 +3,14 @@ function imc(peso, altura) {
 }
 
 console.log(imc(68, 1.65));
+
+function valorFactorial (numero) {
+    let factorial = numero;
+    while(numero > 1){
+        factorial = factorial * (numero - 1);
+        numero--
+    }
+    return factorial;
+}
+
+console.log(valorFactorial(4));
