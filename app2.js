0e4732cb1f455faada7ed5d434409ef692dc354f
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-03-15 21:38:01 -0600

    2024-03-15 Crear una función que reciba un número como parámetro y devuelva el doble de ese número

--- app2.js
+++ app2.js
@@ -6,3 +6,8 @@ function saludoNombre() {
     let nombre = prompt("Dame tu nombre");
     console.log("¡Hola, " + nombre + "!");
 }
+
+function dobleNumero() {
+    let numero = parseInt(prompt("Dame un número"));
+    return numero*2;
+}
