ddd67727dbfec983682b06a0b80741c43e8553d6
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-03-16 19:22:49 -0600

    2024-03-16 Crea una función que muestre en la consola todos los elementos de la lista 'lenguagesDeProgramacion'

--- app4.js
+++ app4.js
@@ -3,3 +3,9 @@ let listaGenerica = [];
 let lenguajesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
 
 lenguajesDeProgramacion.push('Java', 'Ruby', 'GoLang');
+
+function muestra() {
+    return console.log(lenguajesDeProgramacion);
+}
+
+muestra();
